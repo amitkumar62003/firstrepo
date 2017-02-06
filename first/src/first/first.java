22718d5aa10170af4de131fa30f4bef5bc16dba7
Shashwat <Shashwat@ShashwatTyagi>
2017-02-06 13:55:57 +0530

    added hi message

---
+++ first/src/first/first.java
@@ -0,0 +1,11 @@
+package first;
+
+public class first {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+         System.out.println("HELLO");
+         System.out.println("hii");
+	}
+
+}
---
+++ first/src/first/second.java
@@ -0,0 +1,14 @@
+package first;
+
+public class second {
+	int a;
+	public second(int a)
+	{
+	 this.a=a;
+	}
+   public void show()
+   {
+	   System.out.println(a);
+   }
+ }
+
