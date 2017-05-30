eba969a4510bfe65af374819d3a5ca034ff4d88b
root <root@Console>
2017-05-30 18:42:01 +0530

    deleted

--- first/src/first/first.java
+++ first/src/first/first.java
@@ -5,9 +5,8 @@ public class first {
 	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
          System.out.println("HELLO");
-         System.out.println("HEy");
-         System.out.println("HEyyyyy");
-         System.out.println("HEyyyyydsgsdgsdf");         
+
+       
 
 
 	}
